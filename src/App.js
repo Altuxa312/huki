8fdaa767e5a1e4ad7389df10f1ec00ba72fb0383
Altuxa312 <urmatbekovanarbek777@gmail.com>
2022-05-31 19:02:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import images_1 from './img/react.jpg';
+import images_2 from './img/193-1936597_8k-ultra-hd-wallpapers-fresh-8k-ultra-hd.jpg'
+import images_3 from './img/8K-Desktop-Wallpaper-scaled.jpg'
 
 function App() {
+
+  // let [bir, setbir] = useState(0);
+  //  let[eki, seteki] = useState('')
+      const [count, setCuont] = useState(images_2)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     {/* <h1>welcome</h1>
+     <p>{bir}</p>
+    <button onClick={()=> setbir(bir + 1)}>+</button>
+    <button onClick={()=> setbir(bir - 1)}>-</button> */}
+
+    {/* <h2>pishi</h2>
+    <p>{eki}</p>
+    <button onClick={()=> seteki('privet')}>privet</button>
+    <button onClick={()=> seteki('poka')}>poka</button> */}
+
+
+    
+<div> <img src={count}/> </div>
+   <img onClick={()=> seteki(images_1)}/>
+   <img onClick={()=> seteki(images_3)}/>
+  
+
+    
+
+
+    
+  
+
     </div>
   );
 }
